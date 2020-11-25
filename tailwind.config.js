1676426209e1baa28c7d3402ea6d55736dac4361
Kimiaki Kuno <knokmki612@gmail.com>
2020-11-25 20:37:58 +0900

    Upgrade tailwindcss to v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 const typography = require('@tailwindcss/typography');
 
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: [
     'src/script/**/*.js',
     'src/template/**/*.njk',
