2e0e31aa2dd572d4c68bc89b48070b7e50e4180f
Kimiaki Kuno <knokmki612@gmail.com>
2022-03-15 17:46:47 +0900

    chore(eleventy): following breaking changes
    See: https://tailwindcss.com/docs/upgrade-guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,10 @@ const typography = require("@tailwindcss/typography");
 const forms = require("@tailwindcss/forms");
 
 module.exports = {
-  purge: ["src/**/*.js", "src/**/*.njk", "src/**/*.md"],
+  content: ["src/**/*.js", "src/**/*.njk", "src/**/*.md"],
   important: true,
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [typography, forms],
 };
