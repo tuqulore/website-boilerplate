7b0ee1acacc27c7c63c5e0e9e5e10c74c7c7067a
Kimiaki Kuno <knokmki612@gmail.com>
2022-12-28 16:33:42 +0900

    chore(tailwindcss): type support
    See https://github.com/tailwindlabs/tailwindcss/blob/HEAD/CHANGELOG.md#310---2022-06-08

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const typography = require("@tailwindcss/typography");
 const jumpuUi = require("@jumpu-ui/tailwindcss");
 
+/** @type {import('tailwindcss/types').Config} */
 module.exports = {
   content: ["src/**/*.js", "src/**/*.njk", "src/**/*.md"],
   important: true,
