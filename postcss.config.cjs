3885cb660ab094f96ad234bde65a4fe200148ae4
Kimiaki Kuno <knokmki612@gmail.com>
2023-07-05 12:54:24 +0900

    feat: postcss-import-ext-glob の導入 (#390)
    glob パターンでの複数 CSS ファイルのインポートを可能にします

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   map: false,
   from: undefined,
   plugins: {
+    "postcss-import-ext-glob": {},
     "postcss-import": { root: "src/style" },
     "tailwindcss/nesting": {},
     tailwindcss: {},
