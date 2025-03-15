1b444b9caef77e48603163bb9e6208d293e9f2de
Kimiaki Kuno <knokmki612@gmail.com>
2025-03-15 20:32:00 +0900

    WIP

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -4,7 +4,8 @@ import module from "node:module";
 import path from "node:path";
 import url from "node:url";
 import css from "./lib/css.mjs";
-import preact from "./lib/preact.mjs";
+import * as preact from "./lib/preact.mjs";
+import { build, context } from "esbuild";
 
 module.register("./lib/mdx-loader.mjs", url.pathToFileURL("./"));
 module.register("./lib/jsx-loader.mjs", url.pathToFileURL("./"));
@@ -25,9 +26,27 @@ const optimizeImages = async () => {
   }
 };
 
+const transformJsx = async ({ runMode }) => {
+  /** @type {import("esbuild").BuildOptions} */
+  const options = {
+    entryPoints: ["./src/**/*.hydrate.jsx"],
+    outdir: "dist",
+    outbase: "src",
+    jsx: "automatic",
+    jsxImportSource: "preact",
+  };
+  const ctx = await context(options);
+  if (runMode === "build") {
+    await build(options);
+  } else {
+    ctx.watch();
+  }
+};
+
 export default (eleventyConfig) => {
-  eleventyConfig.addTemplateFormats(["jsx", "mdx"]);
-  eleventyConfig.addExtension(["mdx"], preact);
+  eleventyConfig.addTemplateFormats(["hydrate.jsx", "jsx", "mdx"]);
+  eleventyConfig.addExtension("hydrate.jsx", preact.hydrate);
+  eleventyConfig.addExtension(["jsx", "mdx"], preact.template);
   eleventyConfig.addTemplateFormats("css");
   eleventyConfig.addExtension("css", css);
   eleventyConfig.addFilter("date", (date) => date.toLocaleDateString("ja-JP"));
@@ -44,6 +63,7 @@ export default (eleventyConfig) => {
     )]: "/",
   });
   eleventyConfig.on("eleventy.before", optimizeImages);
+  eleventyConfig.on("eleventy.before", transformJsx);
   return {
     dir: {
       input: "src",
