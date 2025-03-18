5c154fe070a24886918f55aafa58663e04595b7d
Kimiaki Kuno <knokmki612@gmail.com>
2025-03-18 18:13:19 +0900

    Migrate css transition code to preact

--- src/_includes/partials/header/desktop.hydrate.jsx
+++ src/_includes/partials/header/desktop.hydrate.jsx
@@ -1,43 +1,62 @@
 import slugify from "slugify";
 import { useState } from "preact/hooks";
+import { twMerge } from "tailwind-merge";
+
+function MenuList(props) {
+  const [open, setOpen] = useState(false);
+  const [visible, setVisible] = useState(open);
+  const handleTransitionStart = () => open && setVisible(true);
+  const handleTransitionEnd = () => !open && setVisible(false);
+  return (
+    <>
+      <button
+        id={`nav-button-${slugify(props.item.name)}`}
+        aria-haspopup="menu"
+        aria-controls={`nav-menu-${slugify(props.item.name)}`}
+        className="jumpu-text-button"
+        type="button"
+        onClick={() => setOpen(!open)}
+      >
+        {props.item.name}
+      </button>
+      <ul
+        id={`nav-menu-${slugify(props.item.name)}`}
+        role="menu"
+        aria-labelledby={`nav-button-${slugify(props.item.name)}`}
+        className={twMerge(
+          "jumpu-card p-2 max-h-[50vh] overflow-y-auto absolute top-full left-1/2 -translate-x-1/2",
+          "transition ease-in-out duration-75 translate-y-2",
+          !open && "opacity-0 translate-y-[2.5%] scale-95",
+          open || visible ? "visible" : "invisible",
+        )}
+        onTransitionStart={handleTransitionStart}
+        onTransitionEnd={handleTransitionEnd}
+      >
+        {props.children}
+      </ul>
+    </>
+  );
+}
 
 export default function Desktop(props) {
-  const [open, setOpen] = useState(false);
   return (
     <nav className="hidden md:block">
       <ul className="flex items-center">
         {props.nav.map((item, itemIndex) =>
           "children" in item ? (
             <li key={`${item.name}-${itemIndex}`} className="relative">
-              <button
-                id={`nav-button-${slugify(item.name)}`}
-                aria-haspopup="menu"
-                aria-controls={`nav-menu-${slugify(item.name)}`}
-                className="jumpu-text-button"
-                type="button"
-                onClick={() => setOpen(!open)}
-              >
-                {item.name}
-              </button>
-              {open && (
-                <ul
-                  id={`nav-menu-${slugify(item.name)}`}
-                  role="menu"
-                  aria-labelledby={`nav-button-${slugify(item.name)}`}
-                  className="jumpu-card p-2 max-h-[50vh] overflow-y-auto absolute top-full left-1/2 -translate-x-1/2 translate-y-2"
-                >
-                  {item.children.map((child, childIndex) => (
-                    <li key={`${child.name}-${childIndex}`} role="menuitem">
-                      <a
-                        className="jumpu-text-button w-full whitespace-nowrap"
-                        href={child.path}
-                      >
-                        {child.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              )}
+              <MenuList item={item}>
+                {item.children.map((child, childIndex) => (
+                  <li key={`${child.name}-${childIndex}`} role="menuitem">
+                    <a
+                      className="jumpu-text-button w-full whitespace-nowrap"
+                      href={child.path}
+                    >
+                      {child.name}
+                    </a>
+                  </li>
+                ))}
+              </MenuList>
             </li>
           ) : (
             <li key={`${item.name}-${itemIndex}`}>
--- src/_includes/partials/header/mobile.hydrate.jsx
+++ src/_includes/partials/header/mobile.hydrate.jsx
@@ -1,12 +1,10 @@
 import { useState } from "preact/hooks";
+import { twMerge } from "tailwind-merge";
 
 export default function Mobile(props) {
   const [open, setOpen] = useState(false);
   return (
-    <div
-      className="flex items-center ml-auto md:hidden"
-      x-init="$watch('open', open => open ? $el.ownerDocument.body.classList.add('overflow-hidden', '[&>:not(#nav-menu-mobile)]:invisible') : $el.ownerDocument.body.classList.remove('overflow-hidden', '[&>:not(#nav-menu-mobile)]:invisible'))"
-    >
+    <div className="flex items-center ml-auto md:hidden">
       <button
         id="nav-button-mobile"
         aria-controls="nav-menu-mobile"
@@ -24,56 +22,58 @@ export default function Mobile(props) {
           Menu
         </span>
       </button>
-      {open && (
-        <nav
-          id="nav-menu-mobile"
-          className="fixed top-0 left-0 w-screen h-screen bg-white overflow-y-auto"
+      <nav
+        id="nav-menu-mobile"
+        className={twMerge(
+          "fixed top-0 left-0 w-screen h-screen bg-white overflow-y-auto",
+          "transition ease-in-out duration-150",
+          open ? "translate-x-0" : "translate-x-full",
+        )}
+      >
+        <button
+          className="fixed top-2 right-4 jumpu-icon-button text-2xl w-12 h-12 group"
+          aria-describedby="nav-tooltip-close"
+          type="button"
+          onClick={() => setOpen(false)}
         >
-          <button
-            className="fixed top-2 right-4 jumpu-icon-button text-2xl w-12 h-12 group"
-            aria-describedby="nav-tooltip-close"
-            type="button"
-            onClick={() => setOpen(false)}
+          <span className="icon-[material-symbols--close]"></span>
+          <span
+            id="nav-tooltip-close"
+            role="tooltip"
+            className="![transform:translate(-50%,_225%)_scale(0)] group-hover:![transform:translate(-50%,_225%)_scale(1)]"
           >
-            <span className="icon-[material-symbols--close]"></span>
-            <span
-              id="nav-tooltip-close"
-              role="tooltip"
-              className="![transform:translate(-50%,_225%)_scale(0)] group-hover:![transform:translate(-50%,_225%)_scale(1)]"
-            >
-              Close
-            </span>
-          </button>
-          <ul className="flex flex-col py-16 px-4">
-            {props.nav.map((item, itemIndex) => (
-              <li key={`${item.name}-${itemIndex}`}>
-                {item.children && (
-                  <section className="mb-6">
-                    <h2 className="text-lg mb-2 ml-4">{item.name}</h2>
-                    <ul>
-                      {item.children.map((child, childIndex) => (
-                        <li key={`${child.name}-${childIndex}`}>
-                          <a
-                            className="jumpu-text-button block"
-                            href={child.path}
-                          >
-                            {child.name}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  </section>
-                )}
-                {item.path && (
-                  <a className="jumpu-text-button block" href={item.path}>
-                    {item.name}
-                  </a>
-                )}
-              </li>
-            ))}
-          </ul>
-        </nav>
-      )}
+            Close
+          </span>
+        </button>
+        <ul className="flex flex-col py-16 px-4">
+          {props.nav.map((item, itemIndex) => (
+            <li key={`${item.name}-${itemIndex}`}>
+              {item.children && (
+                <section className="mb-6">
+                  <h2 className="text-lg mb-2 ml-4">{item.name}</h2>
+                  <ul>
+                    {item.children.map((child, childIndex) => (
+                      <li key={`${child.name}-${childIndex}`}>
+                        <a
+                          className="jumpu-text-button block"
+                          href={child.path}
+                        >
+                          {child.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+              )}
+              {item.path && (
+                <a className="jumpu-text-button block" href={item.path}>
+                  {item.name}
+                </a>
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
